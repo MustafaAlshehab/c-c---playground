7e8f0b54c34c0ffbf7620a445d81180e0868d0ed
MustafaAlshehab <shehab07@gmail.com>
2020-09-29 01:52:33 -0700

    fixed an issue associated with finding_pair_unsorted_hash function

---
+++ pair_elements_sum.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
+#define UNSET -1
+
+int highest_number(int array[], int size) {
+    int highest_number = array[0];
+    for(int i = 1; i < size; i++) {
+        if(array[i] > highest_number) {
+            highest_number = array[i];
+        }
+    }
+    return highest_number;
+}
+
+// Time complexity O(N^2)
+// Space complexity O(1)
+void finding_pair_unsorted(int array[], int size, int target) {
+    for(int i = 0; i < size - 1; i++) {
+        for (int j = i+1; j < size; j++) {
+            if(array[i] + array[j] == target) {
+                printf("\n%d + %d = %d\n", array[i], array[j], target );
+            }
+        }
+    }
+}
+
+// Time complexity O(N)
+// Space complexity O(N)
+void finding_pair_unsorted_hash(int array[], int size, int target) {
+    int hash_array_size = highest_number(array, size);
+    hash_array_size += 1; // to account for the highest number (zero index to highest number)
+    int *hash_array = (int *)malloc(sizeof(int) * hash_array_size);
+    for(int i = 0; i < hash_array_size; i++) {
+        hash_array[i] = UNSET;
+    }
+    for(int i = 0; i < size; i++) {
+        hash_array[array[i]]++;
+        if(target-array[i] >= 0 && hash_array[target-array[i]] != UNSET) {
+            printf("\n%d + %d = %d\n", array[i], target - array[i], target);
+        }
+    }
+}
+
+
+
+
+int main() {
+
+    int array_unsorted[] = {6,3,8,10,16,7,5,2,9,14};
+    int array_sorted[] = {1,1,1,2,8,9,10,10,101,102,102};
+    int target = 10;
+    finding_pair_unsorted_hash(array_unsorted,ARRAY_SIZE(array_unsorted), target);
+    finding_pair_unsorted(array_unsorted, ARRAY_SIZE(array_unsorted), target);
+    finding_pair_sorted(array_sorted, ARRAY_SIZE(array_sorted), target);
+
+    return 0;
+
+}
