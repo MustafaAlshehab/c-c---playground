8e34c645d611304f3488fbc90eb5c046ad61dc00
MustafaAlshehab <shehab07@gmail.com>
2020-10-06 23:00:10 -0700

    added a function to check for palindrome

--- strings/strcmp_and_palindrome.c
+++ strings/strcmp_and_palindrome.c
@@ -18,6 +18,33 @@ int strcmp_clone(const char *string1, const char *string2) {
     return 0;
 }
 
+int strcmp_clone2(const char *string1, const char *string2) {
+    int i = 0;
+    for(; string1[i] != '\0'; i++) {
+        if(string1[i] != string2[i]) {
+            break;
+        }
+    }
+        if(string1[i] > string2[i]) {
+            return 1;
+        } 
+        else if (string1[i] < string2[i]) {
+            return -1;
+        } else {
+            return 0;
+        }
+}
+
+int is_palindrome(const char *string) {
+    int length = strlen(string);
+    for(int i = 0, j = length - 1; i < j; i++, j--) {
+        if(string[i] != string[j]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 int main () {
    char str1[] = "abcdef";
    char str2[] = "abcde";
@@ -31,6 +58,15 @@ int main () {
    } else {
       printf("str1 is equal to str2\n");
    }
-   
+
+   char *str3[] = {"little", "noon", "level"};
+
+   for(int i = 0; i < 3; i++) {
+       if(is_palindrome(str3[i])) {
+           printf("%s is a palindrome\n", str3[i]);
+       } else {
+           printf("%s is NOT a palindrome\n", str3[i]);
+       }
+   }
    return 0;
 }
