e8e301b92aefc9c3952557ab12d43be22bcbb674
MustafaAlshehab <shehab07@gmail.com>
2020-10-15 02:42:14 -0700

    added another implementation of permutation of a string

--- strings/permutation_of_a_string.c
+++ strings/permutation_of_a_string.c
@@ -1,11 +1,27 @@
+/*
+permutation of a string is every arrangement of such a string
+which is n! where n is characters count
+Topics I went through:
+    - Back Tracking
+    - State Space Tree
+*/
+
 #include <stdio.h>
+#include <string.h>
 
 #define UNAVAILABLE 1
 #define AVAILABLE 0
+#define SIZE 30
 
+// Time complexity O(N*N!)
+// Space complexity O(N)
 void str_permutation(const char *str, int tree_level) {
-    static char result[100] = {0};
-    static int index_status[100] = {0};
+    if(strlen(str) > SIZE) {
+        printf("string is too long\n");
+        return;
+    }
+    static char result[SIZE] = {0};
+    static int index_status[SIZE] = {0};
 
     if(str[tree_level] == '\0') {
         result[tree_level] = '\0';
@@ -22,9 +38,31 @@ void str_permutation(const char *str, int tree_level) {
     }
 }
 
+void swap(char *x, char *y) {
+    char temp = *x;
+    *x= *y;
+    *y = temp;
+} 
+
+// Time complexity O(N*N!)
+// Space complexity O(N)
+void str_permutation_2(char *str, int tree_level, int highest_tree_level) {
+
+    if(tree_level == highest_tree_level) {
+        printf("%s\n", str);
+    } else {
+        for(int i = tree_level; str[i] != '\0'; i++) {
+            swap(&str[tree_level], &str[i]);
+            str_permutation_2(str,tree_level+1,highest_tree_level);
+            swap(&str[i], &str[tree_level]);
+        }
+    }
+}
+
 int main() {
     char str1[] = "ABC";
-    str_permutation(str1,0);
+    //str_permutation(str1,0);
+    str_permutation_2(str1,0,strlen(str1));
     
     return 0;
 }
